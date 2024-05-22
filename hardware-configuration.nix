b4c9d139a3ea9a5d1bf8589a9fd3c6100fad9817
Sean Behan <codebam@riseup.net>
2024-05-21 23:57:50 -0400

    enable compression

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -5,7 +5,8 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
@@ -14,19 +15,21 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "UUID=3541a298-17c9-48dd-8202-c9e0b263d2d4";
+    {
+      device = "UUID=3541a298-17c9-48dd-8202-c9e0b263d2d4";
       fsType = "bcachefs";
+      options = [ "compression=lz4" "background_compression=zstd" ];
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/C5ED-F22B";
+    {
+      device = "/dev/disk/by-uuid/C5ED-F22B";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/e53a120f-08de-464c-b46a-4cc4edb0256a"; }
-    ];
+    [{ device = "/dev/disk/by-uuid/e53a120f-08de-464c-b46a-4cc4edb0256a"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
